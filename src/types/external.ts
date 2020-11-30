ba2feec368674941e6cf447e3da9bfdaa2bed56a
Ramesh Nair <ram@hiddentao.com>
2020-11-30 13:41:05 +0000

    chore: add basic docs

--- src/types/external.ts
+++ src/types/external.ts
@@ -1,17 +1,31 @@
-/*
- * This file contains useful type declarations for NPM package consumers
- */
-
 import { Provider, Signer } from 'elrondjs'
 
+/**
+ * Options for getting wallet address.
+ */
 export interface GetWalletAddressOptions {
+  /**
+   * If `true` then user must load a valid wallet if not already done so.
+   */
   mustLoadWallet?: boolean
 }
 
 export interface ErdBox {
+  /**
+   * Set the provider for the widget.
+   */
   setProvider: (provider: Provider) => Promise<void>,
+  /**
+   * Get the current provider used by the widget.
+   */
   getProvider: () => Provider | undefined,
+  /**
+   * Get a signer which can be used to sign transactions.
+   */
   getSigner: () => Signer,
+  /**
+   * Get the current wallet address.
+   */
   getWalletAddress: (options?: GetWalletAddressOptions) => Promise<string>,
 }
 
